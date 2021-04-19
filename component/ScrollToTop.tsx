10dfc49ab1485fc9fb2757085f3f472b2e276969
agkeze1 <engr.ejikeeze@gmail.com>
2021-04-19 12:22:04 +0100

    Started a new Obaforex website pproject

--- component/ScrollToTop.tsx
+++
@@ -1,9 +0,0 @@
-export const ScrollToTop = () => {
-  return (
-    <>
-      <a href="#" data-target="html" className="scroll-to-target scroll-to-top">
-        <i className="fa fa-angle-up"></i>
-      </a>
-    </>
-  );
-};
