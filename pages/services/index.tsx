52f2c3cb4b335913b4edc729a4828294000fc6b0
Otis Chiemezie <otisidevelop@gmail.com>
2021-06-01 08:02:53 +0100

    Updated investment links

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -4,99 +4,79 @@ import services from "../../data/services.json";
 import { Testimonials } from "../../component/Testimonials";
 
 const Services = () => {
-  return (
-    <Layout title="Services">
-      {/* Banner */}
-      <Banner page="Services" />
+    return (
+        <Layout title="Services">
+            {/* Banner */}
+            <Banner page="Services" />
 
-      {/* We Are Here To Help You */}
-      <div className="section-block">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6 col-12 d-none d-md-block">
-              <div className="shadow-primary p-3">
-                <img
-                  src="/img/about/about-img.png"
-                  className="rounded-border shadow-primary"
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="col-md-6 col-12">
-              <div className="pl-30-md">
-                <div className="section-heading text-left mt-5">
-                  <small className="text-scondary">Service We Provide</small>
-                  <h3 className="semi-bold font-size-32">
-                    We Are Here To Help You
-                  </h3>
-                  <div className="section-heading-line line-thin"></div>
-                  <div className="text-content">
-                    <p>
-                      Obaforex is a financial technology and automated trading
-                      and investment service company. We offer sophisticated A.I
-                      driven automated trading solutions for financial markets.
-                      Our mission is to simplify investing and empower investors
-                      with innovations in financial and machine learning
-                      sciences.
-                      <br />
-                      <br />
-                      Our artificial intelligence for algorithmic trading
-                      formulates winning strategies and rigorous risk management
-                      to grow client’s wealth.
-                      <br />
-                      <br />
-                      We develop sophisticated trading models that are able to
-                      continuously learn and improve using advances in A.I,
-                      artificial neural network, machine learning, deep
-                      learning, big data, and analytics to minimize client risk
-                      and maximize returns.
-                    </p>
-                  </div>
+            {/* We Are Here To Help You */}
+            <div className="section-block">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 col-12 d-none d-md-block">
+                            <div className="shadow-primary p-3">
+                                <img src="/img/about/about-img.png" className="rounded-border shadow-primary" alt="" />
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-12">
+                            <div className="pl-30-md">
+                                <div className="section-heading text-left mt-5">
+                                    <small className="text-scondary">Service We Provide</small>
+                                    <h3 className="semi-bold font-size-32">We Are Here To Help You</h3>
+                                    <div className="section-heading-line line-thin"></div>
+                                    <div className="text-content">
+                                        <p>
+                                            Obaforex is a financial technology and automated trading and investment service company. We offer sophisticated A.I driven automated trading solutions for
+                                            financial markets. Our mission is to simplify investing and empower investors with innovations in financial and machine learning sciences.
+                                            <br />
+                                            <br />
+                                            Our artificial intelligence for algorithmic trading formulates winning strategies and rigorous risk management to grow client’s wealth.
+                                            <br />
+                                            <br />
+                                            We develop sophisticated trading models that are able to continuously learn and improve using advances in A.I, artificial neural network, machine learning,
+                                            deep learning, big data, and analytics to minimize client risk and maximize returns.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* What We Do */}
-      <div className="section-backdrop">
-        <div className="overlay-clr">
-          <div className="container py-5">
-            {/* Title and Desc. */}
-            <div className="section-heading text-center mb-5">
-              <h3
-                className="semi-bold font-size-35 pt-5 text-white"
-                style={{ color: "white !important" }}
-              >
-                What We Do
-              </h3>
-              <div className="section-heading-line line-thin"></div>
-              <p style={{ color: "#b1b1b1 !important" }}>
-                Obaforex is here to make you wealthy
-              </p>
             </div>
 
-            <div className="row">
-              {services.map((rec, idx) => (
-                <div key={idx} className="col-xs-12 col-md-6 col-lg-4">
-                  <div className="testmonial-item-5 text-center service-box">
-                    <h5 className="mb-3" style={{ color: "white" }}>
-                      {rec?.title}
-                    </h5>
-                    <p className="text-light-grey">{rec?.desc}</p>
-                  </div>
+            {/* What We Do */}
+            <div className="section-backdrop">
+                <div className="overlay-clr">
+                    <div className="container py-5">
+                        {/* Title and Desc. */}
+                        <div className="section-heading text-center mb-5">
+                            <h3 className="semi-bold font-size-35 pt-5 text-white" style={{ color: "white !important" }}>
+                                What We Do
+                            </h3>
+                            <div className="section-heading-line line-thin"></div>
+                            <p style={{ color: "#b1b1b1 !important" }}>Obaforex is here to make you wealthy</p>
+                        </div>
+
+                        <div className="row">
+                            {services.map((rec, idx) => (
+                                <div key={idx} className="col-xs-12 col-md-6 col-lg-4">
+                                    <div className="testmonial-item-5 text-center service-box">
+                                        <h5 className="mb-3" style={{ color: "white" }}>
+                                            {rec?.title}
+                                        </h5>
+                                        <p className="text-light-grey">{rec?.desc}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
-              ))}
             </div>
-          </div>
-        </div>
-      </div>
 
-      {/* Testimonials */}
-      <Testimonials />
-    </Layout>
-  );
+            {/* Testimonials */}
+            <Testimonials />
+        </Layout>
+    );
 };
 
 export default Services;
--- pages/services/investment/index.tsx
+++ pages/services/investment/index.tsx
@@ -6,79 +6,61 @@ import { TradeViewTop } from "../../../component/TradeViewTop";
 
 const Investment = () => {
   return (
-    <Layout title="Investment">
-      {/* Banner */}
-      <Banner page="Investment" />
+      <Layout title="Investment">
+          {/* Banner */}
+          <Banner page="Investment" />
 
-      {/* Trade View */}
-      <div id="top-bar">
-        <TradeViewTop />
-      </div>
-
-      {/* Invest And Earn */}
-      <div className="section-block grey-bg background-shape-3 border-bottom">
-        <div className="container">
-          {/* Title and Desc. */}
-          <div className="section-heading text-center mb-5">
-            <h3 className="semi-bold font-size-35">Invest And Earn</h3>
-            <div className="section-heading-line line-thin"></div>
-            <p>
-              This type of investment is a fix investment capital, We give juicy
-              returns on investments (ROI) to our investors which ranges from
-              monthly to quarterly. Funds are invested into Foreign Exchange
-              Market, Energy Futures (Oil, Natural Gas) Commodities Futures
-              (Coffee, Cotton, Sugar, Corn, Soya beans, Wheat) for trading
-              purposes and at the end of the month or quarter the investor is
-              given a fix Return on Investment (ROI). Are:
-            </p>
-            <br />
-            {/* Read Our Policy button */}
-            <a
-              href="/documents/investment-policy.pdf"
-              target="_blank"
-              style={{ zIndex: 2 }}
-            >
-              Read Our Policy
-            </a>
-            <br />
-            {/* Join Now and Invest button */}
-            <a
-              href="/documents/investment-policy.pdf"
-              target="_blank"
-              className="button-primary button-md"
-              style={{ marginTop: "40px", zIndex: 2 }}
-            >
-              Join Now and Invest
-            </a>
+          {/* Trade View */}
+          <div id="top-bar">
+              <TradeViewTop />
           </div>
-          {/* Our Investment packages are */}
-          <div className="text-center">
-            <strong style={{ display: "block", margin: "40px 0" }}>
-              Our Investment Packages Are
-            </strong>{" "}
-            <div className="row">
-              {packages.map((rec, idx) => (
-                <div key={idx} className="col-xs-12 col-md-6 mt-2">
-                  <div
-                    className="contact-icon-box callback-block p-5"
-                    style={{ backgroundColor: rec?.bg_color }}
-                  >
-                    <div className="testmonial-box-icon numb">{idx + 1}</div>
-                    <div className="testmonial-box-content text-left">
-                      <h3>{rec?.title}</h3>
-                      <p>{rec?.desc}</p>
-                    </div>
+
+          {/* Invest And Earn */}
+          <div className="section-block grey-bg background-shape-3 border-bottom">
+              <div className="container">
+                  {/* Title and Desc. */}
+                  <div className="section-heading text-center mb-5">
+                      <h3 className="semi-bold font-size-35">Invest And Earn</h3>
+                      <div className="section-heading-line line-thin"></div>
+                      <p>
+                          This type of investment is a fix investment capital, We give juicy returns on investments (ROI) to our investors which ranges from monthly to quarterly. Funds are invested
+                          into Foreign Exchange Market, Energy Futures (Oil, Natural Gas) Commodities Futures (Coffee, Cotton, Sugar, Corn, Soya beans, Wheat) for trading purposes and at the end of
+                          the month or quarter the investor is given a fix Return on Investment (ROI). Are:
+                      </p>
+                      <br />
+                      {/* Read Our Policy button */}
+                      <a href="https://app.obaforex.com/" style={{ zIndex: 2 }}>
+                          Read Our Policy
+                      </a>
+                      <br />
+                      {/* Join Now and Invest button */}
+                      <a href="https://app.obaforex.com/" className="button-primary button-md" style={{ marginTop: "40px", zIndex: 2 }}>
+                          Join Now and Invest
+                      </a>
                   </div>
-                </div>
-              ))}
-            </div>
+                  {/* Our Investment packages are */}
+                  <div className="text-center">
+                      <strong style={{ display: "block", margin: "40px 0" }}>Our Investment Packages Are</strong>{" "}
+                      <div className="row">
+                          {packages.map((rec, idx) => (
+                              <div key={idx} className="col-xs-12 col-md-6 mt-2">
+                                  <div className="contact-icon-box callback-block p-5" style={{ backgroundColor: rec?.bg_color }}>
+                                      <div className="testmonial-box-icon numb">{idx + 1}</div>
+                                      <div className="testmonial-box-content text-left">
+                                          <h3>{rec?.title}</h3>
+                                          <p>{rec?.desc}</p>
+                                      </div>
+                                  </div>
+                              </div>
+                          ))}
+                      </div>
+                  </div>
+              </div>
           </div>
-        </div>
-      </div>
 
-      {/* Get Reliable Signals from Us */}
-      <Signal bgColor="bg-white" />
-    </Layout>
+          {/* Get Reliable Signals from Us */}
+          <Signal bgColor="bg-white" />
+      </Layout>
   );
 };
 
